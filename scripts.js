997e37e3d666c40ef63c291058fee1d728c7601d
duke-newcomb <65268282+duke-newcomb@users.noreply.github.com>
2021-05-30 12:14:28 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
+
+    //Menu Icon slide toggle and change from hamburger to 'x'
     $("#slide").click(function(){
         $("#hidden").slideToggle("slow");
+    });
+    var menuCounter = 0;
+    $("#menuIcon").click(function(){
+        if(menuCounter == 0){
+            $("#menuIcon").fadeOut(function() {
+                $(this).html("&#9747")
+            }).fadeIn();
+            menuCounter++;
+        }
+        else if(menuCounter == 1){
+            $("#menuIcon").fadeOut(function() {
+                $(this).html("&#9776")
+            }).fadeIn(); 
+            menuCounter--;
+        }
     });
 });
